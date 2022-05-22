15798f828e45616f0d44ac1f65aa5af0240eda98
Ewerton Souza <ewertonesouza@gmail.com>
2022-05-22 18:59:19 -0300

    Created results component

--- src/components/results.jsx
+++ src/components/results.jsx
@@ -1,27 +1,24 @@
 import "../assets/styles/results.css"
+import AnimatedNumber from "animated-number-react";
 
 export default function Results() {
     return (
-        <div className="Results row">
-            <div className="Results-column col"></div>
+        <div className="Results mt-3 mb-3">
+            <div className="ResultsColumn container">
+                <div className="ResultsValue">
+                    <strong className="ResultsValueNumber">
+                        <AnimatedNumber value={1.01} duration={5000} formatValue={(value) => `+ ${value.toFixed(2)}%`} />
+                    </strong>
+                    <br />últimos 30d
+                </div>
+                <div className="ResultsValue">
+                    <strong className="ResultsValueNumber">
+                        <AnimatedNumber value={13.1} duration={5000} formatValue={(value) => `+ ${value.toFixed(1)}%`} />
+                    </strong>
+                    <br />últimos 12m
+                </div>
+            </div>
             <div className="Results-column col"></div>
         </div>
     )
 }
-
-
-// <div className="Banner row">
-//     <div className="Banner-column col">
-//         <div className="Banner-title mb-3">Inteligência&nbsp;Artificial na Bolsa&nbsp;de&nbsp;Valores</div>
-//         <div className="Banner-content">
-//             <p>Corujo é uma rede neural que coleta diariamente dados de diversos tipos de ativos como ações, índices, fundos de investimento, FOREX, <i>commodities</i> & criptomoedas.</p>
-//             <p>Corujo gera uma distribuição otimizada e diversificada que tende a maximizar o lucro em D+1, porém fornecendo previsões para períodos maiores também.</p>
-//         </div>
-//     </div>
-//     <div className="Banner-column col">
-//         <figure>
-//             <img src={cute_owl} className="rounded float-right" width="100%" alt="" />
-//             <figcaption>Olha que coisa linda!</figcaption>
-//         </figure>
-//     </div>
-// </div>
